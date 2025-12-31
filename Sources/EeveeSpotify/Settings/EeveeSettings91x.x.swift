e9163a0509936e37a1c43557bda0d8c2cb4afc45
Camden <camdenheil6711@icloud.com>
2025-12-31 01:11:14 -0500

    Fix: Use separate handler class for gesture target-action

--- Sources/EeveeSpotify/Settings/EeveeSettings91x.x.swift
+++ Sources/EeveeSpotify/Settings/EeveeSettings91x.x.swift
@@ -5,35 +5,18 @@ import UIKit
 // Settings integration for 9.1.x - uses triple-tap on status bar area to show version info
 struct V91SettingsIntegrationGroup: HookGroup { }
 
-// Hook UIWindow to detect triple-tap on status bar area
-class UIWindowTripleTapHook: ClassHook<UIWindow> {
-    typealias Group = V91SettingsIntegrationGroup
-    
-    static var hasSetupGesture = false
-    
-    func didMoveToSuperview() {
-        orig.didMoveToSuperview()
-        
-        // Only setup once
-        guard !Self.hasSetupGesture, target.superview != nil else { return }
-        Self.hasSetupGesture = true
-        
-        // Add a triple-tap gesture recognizer to the window
-        let tripleTap = UITapGestureRecognizer(target: self, action: #selector(handleTripleTap(_:)))
-        tripleTap.numberOfTapsRequired = 3
-        target.addGestureRecognizer(tripleTap)
-        
-        NSLog("[EeveeSpotify] Triple-tap gesture added to UIWindow for version info")
-    }
-    
+// Helper class to handle gesture recognition (not a hook, just a regular class)
+class TripleTapGestureHandler: NSObject {
     @objc func handleTripleTap(_ gesture: UITapGestureRecognizer) {
-        let tapLocation = gesture.location(in: target)
+        guard let window = gesture.view as? UIWindow else { return }
+        
+        let tapLocation = gesture.location(in: window)
         
         // Only trigger if tapped in the top 60 points (status bar area)
         guard tapLocation.y < 60 else { return }
         
         // Find the top-most view controller
-        guard var topController = target.rootViewController else { return }
+        guard var topController = window.rootViewController else { return }
         
         while let presentedViewController = topController.presentedViewController {
             topController = presentedViewController
@@ -66,5 +49,28 @@ class UIWindowTripleTapHook: ClassHook<UIWindow> {
         topController.present(alert, animated: true)
         
         NSLog("[EeveeSpotify] Version info alert presented")
+    }
+}
+
+// Hook UIWindow to add triple-tap gesture
+class UIWindowTripleTapHook: ClassHook<UIWindow> {
+    typealias Group = V91SettingsIntegrationGroup
+    
+    static var hasSetupGesture = false
+    static var gestureHandler = TripleTapGestureHandler()
+    
+    func didMoveToSuperview() {
+        orig.didMoveToSuperview()
+        
+        // Only setup once
+        guard !Self.hasSetupGesture, target.superview != nil else { return }
+        Self.hasSetupGesture = true
+        
+        // Add a triple-tap gesture recognizer to the window
+        let tripleTap = UITapGestureRecognizer(target: Self.gestureHandler, action: #selector(TripleTapGestureHandler.handleTripleTap(_:)))
+        tripleTap.numberOfTapsRequired = 3
+        target.addGestureRecognizer(tripleTap)
+        
+        NSLog("[EeveeSpotify] Triple-tap gesture added to UIWindow for version info")
     }
 }
